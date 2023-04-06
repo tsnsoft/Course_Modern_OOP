7f8b226183ef2c812759ca27fd0c2854bf1a7326
Talipov S.N <proffix4@gmail.com>
2023-04-06 20:08:15 +0600

    update

---
+++ Тема 5 - Работа с Preferences/TSN_JAVA_Preferences_matrix/README.md
@@ -0,0 +1,20 @@
+# TSN_JAVA_Preferences_matrix
+Пример работы с Preferences на Java в NetBeans
+
+```
+Laboratory work
+Task: MAX <> MIN
+Matrix:
+59 46 9 30 99 34 
+94 46 57 17 92 25 
+95 20 32 8 64 75 
+7 7 99 54 42 78 
+30 90 39 74 64 30 
+New matrix:
+59 46 9 30 7 34 
+94 46 57 17 92 25 
+95 20 32 8 64 75 
+99 7 99 54 42 78 
+30 90 39 74 64 30 
+```
+![srcreenshot](screenshot.png)
---
+++ Тема 5 - Работа с Preferences/TSN_JAVA_Preferences_matrix/src/tsn_java_preferences/TSN_JAVA_Preferences_matrix.java
@@ -0,0 +1,74 @@
+package tsn_java_preferences;
+
+import java.util.prefs.Preferences;
+
+public class TSN_JAVA_Preferences_matrix {
+
+    public static void main(String[] args) {
+        Preferences node = Preferences.userRoot().node("tsn_sp_lab3");
+
+        final int R = 5; // Количество строк
+        final int C = 6; // Количество столбцов
+        int m[][] = new int[R][C]; // Массив
+        int k;
+
+        // Вывод данных о задании на экран
+        System.out.println("Laboratory work");
+        System.out.println("Task: MAX <> MIN");
+
+        // Заполняем массив случайными числами
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                k = (int) Math.round(Math.random() * 101);
+                m[i][j] = k;
+            }
+        }
+
+        // Вывод на экран изначального массива
+        System.out.println("Matrix:");
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.print(String.valueOf(m[i][j]) + " ");
+
+            }
+            System.out.println();
+        }
+
+        // Реализация алгоритма варианта задания 
+        int min = m[0][0], max = m[0][0], maxi = 0, maxj = 0, mini = 0, minj = 0;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                k = m[i][j];
+                if (k > max) {
+                    max = k;
+                    maxi = i;
+                    maxj = j;
+                }
+                if (k < min) {
+                    min = k;
+                    mini = i;
+                    minj = j;
+                }
+            }
+        }
+        k = m[maxi][maxj];
+        m[maxi][maxj] = m[mini][minj];
+        m[mini][minj] = k;
+
+        // Заносим данные массива в реестр
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                node.putInt("K" + i + "," + j, m[i][j]);
+            }
+        }
+
+        // Считываем данные массива из реестра и выводим их на экран
+        System.out.println("New matrix:");
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                System.out.print(node.getInt("K" + i + "," + j, 0) + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
